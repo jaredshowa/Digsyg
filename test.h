b14a32a7da25df94429229ffb3d8b2988bce9b43
sondrern <sondrern@gmail.com>
2016-09-22 12:20:17 +0200

    Create test.h

---
+++ test.h
@@ -0,0 +1,24 @@
+unsigned int myAdd(unsigned int a, unsigned int b);
+
+unsigned int myMult (unsigned int a, unsigned int b);
+
+unsigned int modolo (unsigned long int t, unsigned int n, unsigned int k);
+
+unsigned int findbit(unsigned int x);
+unsigned int blakley(unsigned int a, unsigned int b, unsigned int n);
+
+
+unsigned int binMethod(unsigned int m, unsigned int e, unsigned int n);
+
+unsigned int modinv(unsigned int u, unsigned int v);
+
+
+unsigned int evenModExp(unsigned int a, unsigned int e, unsigned int n);
+
+
+unsigned int monpro(unsigned int x,unsigned int y,unsigned int n, unsigned int n2,unsigned int  r);
+unsigned int modexp(unsigned int m, unsigned int e, unsigned int n);
+
+void printbitwise(unsigned int x);
+
+unsigned int montgomery(unsigned int m, unsigned int e, unsigned int n);
